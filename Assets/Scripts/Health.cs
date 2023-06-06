a1f43fffdd7c8a03124bd3f7a66b4a6112a2c094
min.shuun <min.shuun@gmail.com>
2023-06-06 16:11:01 +0700

    13. Screen Shake

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,14 @@ public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
     [SerializeField] ParticleSystem hitEffect;
+    [SerializeField] bool applyCameraShake;
+
+    CameraShake cameraShake;
+
+    private void Awake() 
+    {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
@@ -14,8 +23,17 @@ public class Health : MonoBehaviour
         {
             TakeDamage(damageDealer.GetDamage());
             PlayHitEffect();
+            ShakeCamera();
             damageDealer.Hit();
         }    
+    }
+
+    private void ShakeCamera()
+    {
+        if(cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
+        }
     }
 
     void TakeDamage(int value)
