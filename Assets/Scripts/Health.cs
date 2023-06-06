2cedcb19e74748626c5d60af9189dad7bff2b83c
min.shuun <min.shuun@gmail.com>
2023-06-06 14:40:07 +0700

    12. Particles System

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
@@ -12,6 +13,7 @@ public class Health : MonoBehaviour
         if(damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
             damageDealer.Hit();
         }    
     }
@@ -22,6 +24,16 @@ public class Health : MonoBehaviour
         if(health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect () 
+    {
+        if(hitEffect != null)
+        {
+            // hitEffect.Play
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
